44e616e6f4382782087c726e686d120ddf33b1c2
Roger Hsiao <74138499+yzslay@users.noreply.github.com>
2022-04-12 17:32:51 +0800

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -11,13 +11,13 @@
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">功能一</a>
+          <a class="nav-link" href="#">飲料試算機</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">功能二</a>
+          <a class="nav-link" href="#">關燈統計表</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">後台</a>
         </li>
       </ul>
     </div>
