2baa459f617f15f7532ca2b434db24515c293f80
Roger Hsiao <74138499+yzslay@users.noreply.github.com>
2022-04-15 17:17:14 +0800

    更新

--- src/components/DrinksCal.vue
+++ src/components/DrinksCal.vue
@@ -1,4 +1,12 @@
 <template>
+  <div>
+      什麼有更多人想喝飲料  
+  <button value="+" class="btn btn-primary">++</button>
+      什麼奶茶的力量在消退了
+  <button value="+" class="btn btn-danger">--</button>
+      開始計算奶茶的戰鬥力吧
+  <button value="" class="btn btn-info">計算</button>
+   </div>
   <div class="container"> 
   
   <table class="table">
@@ -14,17 +22,17 @@
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-                              
+    <tr v-for="drink in drinkList" :key="drink.index">
+      <th scope="row">{{drink.index}}</th>
+      <td>{{drink.name}}</td>
+      <td>{{drink.drinks}}</td>
+      <td>{{drink.price}}</td>
+      <td>{{drink.quanity}}</td>
+      <td>{{drink.price*drink.quanity}}</td>
+      <td></td>
+                            
     </tr>
-    <tr>
+    <!-- <tr>
       <th scope="row">2</th>
       <td>Jacob</td>
       <td>Thornton</td>
@@ -32,7 +40,7 @@
         <td>@fat</td>
           <td>@fat</td>
             <td>@fat</td>
-    </tr>
+    </tr> -->
   </tbody>
 </table>
 <form>
@@ -74,8 +82,19 @@
 
 <script>
 export default {
-
+    data() { 
+        return {
+        drinkList: [{ 
+        index:0,
+        name: '小明', 
+        drinks: '珍珠奶茶', 
+        price: '50', 
+        quanity: 1,
+        }],
 }
+},
+}
+
 </script>
 
 <style>
